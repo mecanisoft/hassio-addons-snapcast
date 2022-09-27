29a61201aec27e5a60bba2851ee7e0a49c01c8a6
Xlagrap <lagrap@gmail.com>
2022-09-27 13:37:03 +0200

    Update run.sh

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -5,6 +5,4 @@ mkdir -p /share/snapcast
 
 bashio::log.info "Starting SnapServer..."
 
-/usr/bin/snapserver -c /share/snapcast/snapserver.conf &
-
-cvlc -I telnet --telnet-port 4212 --telnet-password testest --no-video --aout afile --audiofile-file /share/snapfifo/vlc-tous &
+/usr/bin/snapserver -c /share/snapcast/snapserver.conf
