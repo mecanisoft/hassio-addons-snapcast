077c2948d0507591bfed049b7fb6727ce9ef3a17
Xlagrap <lagrap@gmail.com>
2022-09-30 19:11:06 +0200

    Update run.sh

--- mimic3/run.sh
+++ mimic3/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd mimic3
+
+file="/mimic3/mimic3_tts/config.py"
+searchrate="sample_rate: int = 22050"
+replacerate="sample_rate: int = 44100"
+sed -i "s/$searchrate/$replacerate/" $file
+searchchan="channels: int = 1"
+replacechan="channels: int = 2"
+sed -i "s/$searchchan/$replacechan/" $file
+
 source ./.venv/bin/activate
 mimic3-server
