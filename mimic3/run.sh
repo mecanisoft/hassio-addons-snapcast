41b47a5cb64c8f3ba39889d39c8a9ef22a567098
Xlagrap <lagrap@gmail.com>
2022-10-01 16:42:19 +0200

    Update run.sh

--- mimic3/run.sh
+++ mimic3/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-file="/mimic3/opentts_abc/__init__.py"
+file1="/mimic3/opentts_abc/__init__.py"
+file2/mimic3/mimic3_http/synthesis.py
 searchrate="wav_file.setframerate(22050)"
 replacerate="wav_file.setframerate(44100)"
-sed -i "s/$searchrate/$replacerate/" $file
+sed -i "s/$searchrate/$replacerate/" $file1
+sed -i "s/$searchrate/$replacerate/" $file2
 
 searchchan="wav_file.setnchannels(1)"
 replacechan="wav_file.setnchannels(2)"
-sed -i "s/$searchchan/$replacechan/" $file
+sed -i "s/$searchchan/$replacechan/" $file1
+sed -i "s/$searchrate/$replacerate/" $file2
 
 cd mimic3
 source ./.venv/bin/activate
