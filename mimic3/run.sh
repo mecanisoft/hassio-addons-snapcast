d0ae638d8917398fa8ea000916c9532a694acc77
Xlagrap <lagrap@gmail.com>
2022-10-01 16:20:04 +0200

    Update run.sh

--- mimic3/run.sh
+++ mimic3/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 cd mimic3
-
-file="/mimic3/mimic3_tts/tts.py"
-searchrate="sample_rate: int = 22050"
-replacerate="sample_rate: int = 44100"
-sed -i "s/$searchrate/$replacerate/" $file
-#searchchan="channels: int = 1"
-#replacechan="channels: int = 2"
-#sed -i "s/$searchchan/$replacechan/" $file
-
 source ./.venv/bin/activate
+
+mimic3-download fr_FR/siwis_low
+
+file="/root/.local/share/mycroft/mimic3/voices/fr_FR/siwis_low/config.json"
+searchrate='"sample_rate": 22050'
+replacerate='"sample_rate": 44100'
+sed -i "s/$searchrate/$replacerate/" $file
+
+file="/root/.local/share/mycroft/mimic3/voices/fr_FR/siwis_low/config.json"
+searchchan='"channels": 1'
+replacechan='"channels": 2'
+sed -i "s/$searchchan/$replacechan/" $file
+
 mimic3-server
